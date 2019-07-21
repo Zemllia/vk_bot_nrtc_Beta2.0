53afc2bd2e16d3c54bb7d29d580b660fb30daeab
SergeyBulanov2001 <sergey.bulanov.2001@bk.ru>
2019-07-21 17:14:58 +0300

    Написал отправитель сообщений

--- message_sender.py
+++ message_sender.py
@@ -1,14 +1,17 @@
 from time import time, sleep
 from datetime import datetime
+from random import randint
 
 from requests import post
 
 from threading import Thread
 
+import config
+
 
 class Sender:
-    def __init__(self, vk_token):
-        self.vk_token = vk_token
+    def __init__(self):
+        self.url = 'https://api.vk.com/method/execute?code=return [%s];&v=5.101&access_token=' + config.vk_token
 
         self.works = True
         self.turn_vip = []
@@ -16,56 +19,57 @@ class Sender:
 
         self.time = time()
 
-    def send_message(self, message):
-        attributes = ''
-        for key in message:
-            attributes += '%s=%s&' % (key, message[key])
-
-        answer = post('https://api.vk.com/method/messages.send?%sv=5.38&access_token=%s' % (attributes, self.vk_token)).json()
-        return answer
-
     def sender(self):
         while True:
-            if not self.turn_vip == []:
-                message = self.turn_vip[0]
-                vip = True
+            if not self.turn == []:
+                attributes = ''
+                messages = self.turn[:25]
 
-            elif not self.turn == []:
-                message = self.turn[0]
-                vip = False
+                for message in messages:
+                    message['random_id'] = randint(-100000000, 1000000000000000)
+                    attributes += 'API.messages.send(%s),' % str(message)
 
+                """
+                if (time() - self.time) < 0.3333334: sleep(0.3333334 - (time() - self.time))
+                print(time() - self.time)
+                self.time = time()
+                """
+
+                print('Sender/%s/%ix/messages=%s//' % (str(datetime.now()), len(messages),  messages))
+                answer = post(self.url % attributes).json()
+
+                del self.turn[:len(messages)]
             else:
                 sleep(0.01)
                 if not self.works: break
                 continue
 
-            """
-            if (time() - self.time) < 0.3333334: sleep(0.3333334 - (time() - self.time))
-            print(time() - self.time)
-            self.time = time()
-            """
-
-            print('Sender/%s/id=%s/message=%s' % (str(datetime.now()), message['peer_id'], message['message']))
-            result = self.send_message(message)
-
-            if vip: del self.turn_vip[self.turn_vip.index(message)]
-            else: del self.turn[self.turn.index(message)]
-
     def start(self):
         self.works = True
-        Thread(target=self.sender, name='Sender')
+        Sender = Thread(target=self.sender, name='Sender')
+        Sender.start()
 
     def stop(self):
         self.works = False
 
-    def add(self, peer_id, message=None, keyboard=None, attachment=None, priority=False):
+    def add(self, peer_id, message='', keyboard=None, attachment=None, priority=False):
         attributes = {'peer_id': peer_id}
 
-        if message != None: attributes['message'] = message
+        attributes['message'] = message
         if keyboard != None: attributes['keyboard'] = keyboard
         if attachment != None: attributes['attachment'] = attachment
 
-        if priority:
-            self.turn_vip.append(attributes)
-        else:
-            self.turn.append(attributes)
+        self.turn.append(attributes)
+
+
+if __name__ == '__main__':
+    Sender = Sender()
+    Sender.start()
+    i = 0
+    while True:
+        #id = 265868386
+        id = 284964657
+        m = 'Земля лох'
+
+        Sender.add(id, message=m)
+        i += 1
